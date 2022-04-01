a075524fc64cf8706a8bf19ae47f7c3288422dc3
Duc Nguyen Huu <duc.nguyen@kyanon.digital>
2022-04-01 14:30:19 +0700

    commit3

---
+++ src/templates/HomeTemplate/Layout/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className="container-fluid ">
+          <span>Copyright By VinaWatch©</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
